598053041879f774863a2cad9812a34b51e820da
lqiang2003cn <randycoq@163.com>
2023-08-06 14:23:14 +0800

    c

--- src/make_marker.cpp
+++ src/make_marker.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <aruco/marker.h>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, char **argv)
+int main()
 {
     aruco::Marker marker = aruco::Marker(123);
     cv::Mat markerImage;
     marker.draw(markerImage, NULL, 10, true , true);
+    cv::imwrite("test4.jpg", markerImage);
     std::cout << "running";
 }
